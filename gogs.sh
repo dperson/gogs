4de7b2ccb984b0a23b0fdb1cbf3e1eceb8023ed3
David Personette <dperson@gmail.com>
2015-10-30 05:35:26 -0400

    gogs.sh derp fix changing GID

--- gogs.sh
+++ gogs.sh
@@ -65,7 +65,7 @@ shift $(( OPTIND - 1 ))
 
 [[ "${TZ:-""}" ]] && timezone "$TZ"
 [[ "${USERID:-""}" =~ ^[0-9]+$ ]] && usermod -u $USERID gogs
-[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && usermod -g $GROUPID gogs
+[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID gogs
 
 chown -Rh gogs. /opt/gogs 2>&1 | grep -iv 'Read-only' || :
 
